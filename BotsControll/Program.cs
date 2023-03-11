ce77e7f73d9080c34be632c5e2305aaecf3a8b14
kukkich <vitia.iezhovqqqq@gmail.com>
2023-03-11 12:42:14 +0700

    Добавьте файлы проекта.

---
+++ BotsControll/Program.cs
@@ -0,0 +1,45 @@
+
+namespace BotsControll
+{
+    public class Program
+    {
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSignalR();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public static void Configure(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+        }
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            ConfigureServices(builder.Services);
+
+            var app = builder.Build();
+            Configure(app);
+
+            app.Run();
+        }
+    }
+}
